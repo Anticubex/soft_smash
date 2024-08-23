9dc295b5e6e68b7f53362348b18e8b38e4ece161
Anticubex <teofontana00@gmail.com>
2024-08-23 11:11:49 -0700

    Add shape matching to the debug drawing

--- src/debug.c
+++ src/debug.c
@@ -1,6 +1,25 @@
 #include "debug.h"
+#include <stdlib.h>
 
 void DrawSoftbody_debug(SoftBody sb) {
+        // Draw shape
+
+        Matrix shapeMatrix = MatrixIdentity();
+        float sinA = sinf(sb.shapeRotation);
+        float cosA = cosf(sb.shapeRotation);
+        shapeMatrix.m0 = cosA;
+        shapeMatrix.m1 = sinA;
+        shapeMatrix.m4 = -sinA;
+        shapeMatrix.m5 = cosA;
+        Vector2 *shapePos = alloca(sizeof(Vector2) * sb.numPoints);
+        for (int i = 0; i < sb.numPoints; i++) {
+                shapePos[i] = Vector2Add(Vector2Transform(sb.shape[i], shapeMatrix), sb.shapePosition);
+        }
+        for (int i = 0; i < sb.numPoints; i++) {
+                // Line
+                DrawLineEx(shapePos[i], shapePos[(i + 1) % sb.numPoints], 0.1f, GRAY);
+                DrawLineEx(shapePos[i], sb.pointPos[i], 0.15f, YELLOW);
+        }
         // Draw Springs
         for (int i = 0; i < sb.numSprings; i++) {
                 DrawLineEx(sb.pointPos[sb.springA[i]], sb.pointPos[sb.springB[i]], 0.15f, YELLOW);
