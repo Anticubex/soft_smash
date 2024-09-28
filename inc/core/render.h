e8e45eeae3348d217bad21f6606bfa9af156f83a
Anticubex <teofontana00@gmail.com>
2024-09-28 14:52:24 -0700

    Fill-based Rendering Is now Functional

--- inc/core/render.h
+++ inc/core/render.h
@@ -4,33 +4,12 @@ typedef struct SoftBodyRenderer {
         // A surface following how to render the softbody
         int num;
         int *pts;
-        Vector2 *uv;
+        Color fillColor;
+        Color borderColor;
+        float thickness;
 } SoftBodyRenderer;
+void freeRenderer(SoftBodyRenderer *rend);
 
 void renderSoftbody(SoftBody sb, SoftBodyRenderer rend);
 
-typedef struct PolyNode {
-        int root;
-        int length;
-        struct PolyNode *next;
-} PolyNode;
-
-typedef struct Halfplane {
-        Vector2 p;
-        Vector2 pq;
-        float angle;
-} Halfplane;
-
-float VecCross(Vector2 a, Vector2 b);
-Halfplane createHalfplane(Vector2 a, Vector2 b);
-bool outHalfplane(Halfplane plane, Vector2 point);
-bool isStarShaped(Vector2 *vertices, int *poly, int num, Vector2 *center);
-PolyNode *splitPoly(Vector2 *points, int num);
-
-void merge(Vector2 *points, int *indices, int left, int mid, int right, int *temp);
-
-void mergeSortHelper(Vector2 *points, int *indices, int left, int right, int *temp);
-
-void sortY(Vector2 *points, int num, int *dest);
-
-void DrawTexturePoly(Texture2D texture, Vector2 texcenter, Vector2 center, Vector2 *points, Vector2 *texcoords, int pointCount, Color tint);
+void autogenerateRendererFromSurface(SoftBody sb, SoftBodyRenderer *rend);
--- src/core/render.c
+++ src/core/render.c
@@ -1,161 +1,54 @@
 #include <core/render.h>
-#include <math.h>
+#include <libtess2/tesselator.h>
 #include <raylib.h>
-#include <rlgl.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void renderSoftbody(SoftBody sb, SoftBodyRenderer rend) {
-}
 
-float VecCross(Vector2 a, Vector2 b) {
-        return a.x * b.y - a.y * b.x;
-}
+        // This alloca means this method is (supposedly) only really "safe"
+        // for <512 or so vertices. tbh, if you're using more than that, that's
+        // a you issue.
+        Vector2 *vertexArray = alloca(sizeof(Vector2) * (rend.num + 1));
+        for (int i = 0; i < rend.num; i++) {
+                vertexArray[i] = sb.pointPos[rend.pts[i]];
+        }
+        vertexArray[rend.num] = sb.pointPos[rend.pts[0]]; // For the border spline
 
-Halfplane createHalfplane(Vector2 a, Vector2 b) {
-        Vector2 pq = Vector2Subtract(b, a);
-        return (Halfplane){
-            .p = a,
-            .pq = pq,
-            .angle = atan2(pq.y, pq.x),
-        };
-}
+        TESStesselator *tessellator = tessNewTess(NULL);
+        tessSetOption(tessellator, TESS_CONSTRAINED_DELAUNAY_TRIANGULATION, 1);
+        tessAddContour(tessellator, 2, vertexArray, sizeof(Vector2), rend.num);
 
-int compareHalfplanes(const void *vpa, const void *vpb) {
-        Halfplane *a = vpa;
-        Halfplane *b = vpb;
-        return (a->angle > b->angle) - (a->angle < b->angle);
-}
+        tessTesselate(tessellator, TESS_WINDING_ODD, TESS_POLYGONS, 3, 2, NULL);
 
-bool outHalfplane(Halfplane plane, Vector2 point) {
-        return VecCross(plane.pq, Vector2Subtract(point, plane.p)) < 0.f;
-}
+        int vertexCount = tessGetVertexCount(tessellator);
+        const Vector2 *vertices = (const Vector2 *)tessGetVertices(tessellator);
+        int indexCount = tessGetElementCount(tessellator) * 3;
+        int numTris = tessGetElementCount(tessellator);
+        const int *indices = tessGetElements(tessellator);
 
-Vector2 intersection(Halfplane a, Halfplane b) {
-        float alpha = VecCross(Vector2Subtract(b.p, a.p), b.pq) /
-                      VecCross(a.pq, b.pq);
-        return Vector2Add(a.p, Vector2Scale(a.pq, alpha));
-}
-
-bool isStarShaped(Vector2 *vertices, int *poly, int num, Vector2 *center) {
-        // First, get the intersection of all the halfplanes
-
-        // Generate the halfplanes
-        Halfplane *planes = alloca(sizeof(Halfplane) * num);
-
-        // planes[0] = createHalfplane((Vector2){FP_INFINITE, FP_INFINITE}, (Vector2){-FP_INFINITE, FP_INFINITE});
-        // planes[1] = createHalfplane((Vector2){-FP_INFINITE, FP_INFINITE}, (Vector2){-FP_INFINITE, -FP_INFINITE});
-        // planes[2] = createHalfplane((Vector2){-FP_INFINITE, -FP_INFINITE}, (Vector2){FP_INFINITE, -FP_INFINITE});
-        // planes[3] = createHalfplane((Vector2){FP_INFINITE, -FP_INFINITE}, (Vector2){FP_INFINITE, FP_INFINITE});
-
-        for (int i = 0; i < num; i++) {
-
-                planes[i] = createHalfplane(vertices[poly[i]], vertices[poly[(i + 1) % num]]);
+        for (int t = 0, i = 0; t < numTris; t++, i += 3) {
+                DrawTriangle(
+                    vertices[indices[i + 2]],
+                    vertices[indices[i + 1]],
+                    vertices[indices[i + 0]],
+                    rend.fillColor);
         }
 
-        // sort them and stuff
-        qsort(planes, num, sizeof(Halfplane), compareHalfplanes);
-        int *deque = alloca(sizeof(int) * num);
-        int front = 0, back = 0, len = 1;
+        DrawSplineLinear(vertexArray, rend.num + 1, rend.thickness, rend.borderColor);
 
-        deque[0] = 0;
-
-        for (int i = 1; i < num; i++) {
-
-                // Remove from the back of the deque while last half-plane is redundant and
-                // the front of the deque while the first half-plane is redundant.
-                // * Note that at any step len>=2, so just incrementing/decrementing is safe.
-                while (len > 1 &&
-                       outHalfplane(planes[i], intersection(planes[deque[back]],
-                                                            planes[deque[back - 1]]))) {
-                        back--;
-                        len--;
-                }
-                while (len > 1 &&
-                       outHalfplane(planes[i], intersection(planes[deque[front]],
-                                                            planes[deque[front + 1]]))) {
-                        front++;
-                        len--;
-                }
-
-                // Special case check: Parallel half-planes
-                // * Note: This is graphics, so epsilon doesn't have to be perfect
-                if (len > 0 && fabsl(VecCross(planes[i].pq, planes[back].pq)) < 0.00001F) {
-                        // Opposite parallel half-planes that ended up checked against each other.
-                        if (Vector2DotProduct(planes[i].pq, planes[deque[back]].pq) < 0.0)
-                                return false;
-
-                        // Same direction half-plane: keep only the leftmost half-plane.
-                        if (outHalfplane(planes[i], planes[deque[back]].p)) {
-                                back--; // Again, len > 1 so safe (or at least it will be after pushing)
-                                len--;
-                        } else
-                                continue;
-                }
-
-                // Add new half-plane
-                deque[++back] = i;
-                ++len;
-        }
-
-        // Final cleanup: Check half-planes at the front against the back and vice-versa
-        while (len > 2 && outHalfplane(planes[deque[0]], intersection(planes[deque[back]], planes[deque[back - 1]]))) {
-                back--;
-                len--;
-        }
-        while (len > 2 && outHalfplane(planes[deque[back]], intersection(planes[deque[front]], planes[deque[front + 1]]))) {
-                front++;
-                len--;
-        }
-
-        if (len < 3)
-                return false;
-
-        // Average out the intersection points to get a nice center
-
-        *center = (Vector2){0.f, 0.f};
-        for (int i = front; i < back; i++) {
-                *center = Vector2Add(*center, intersection(planes[deque[i]], planes[deque[i + 1]]));
-        }
-        *center = Vector2Add(*center, intersection(planes[deque[back]], planes[deque[front]]));
-        *center = Vector2Scale(*center, 1.f / len);
-
-        return 0;
+        tessDeleteTess(tessellator);
 }
 
-// Splits a polygon into a list of triangles
-PolyNode *splitPoly(Vector2 *points, int num) {
-        // Idea: keep chunking off triangles off the edges until the remnants are star-shaped
-        // The star-shaped check helps to keep the texture from being disgustingly squished
-        // I think this is basically an application of the two ears theorem
+void freeRenderer(SoftBodyRenderer *rend) {
+        MemFree(rend->pts);
+        rend->num = 0;
 }
 
-// Draw textured polygon, defined by vertex and texture coordinates
-// NOTE: Polygon center must have straight line path to all points
-// without crossing perimeter, points must be in anticlockwise order
-// i.e. star shaped w/ `center` in kernel
-void DrawTexturePoly(Texture2D texture, Vector2 texcenter, Vector2 center, Vector2 *points, Vector2 *texcoords, int pointCount, Color tint) {
-        rlSetTexture(texture.id);
-
-        // Texturing is only supported on RL_QUADS
-        rlBegin(RL_QUADS);
-
-        rlColor4ub(tint.r, tint.g, tint.b, tint.a);
-
-        for (int i = 0; i < pointCount - 1; i++) {
-                rlTexCoord2f(texcenter.x, texcenter.y);
-                rlVertex2f(center.x, center.y);
-
-                rlTexCoord2f(texcoords[i].x, texcoords[i].y);
-                rlVertex2f(points[i].x + center.x, points[i].y + center.y);
-
-                rlTexCoord2f(texcoords[i + 1].x, texcoords[i + 1].y);
-                rlVertex2f(points[i + 1].x + center.x, points[i + 1].y + center.y);
-
-                rlTexCoord2f(texcoords[i + 1].x, texcoords[i + 1].y);
-                rlVertex2f(points[i + 1].x + center.x, points[i + 1].y + center.y);
-        }
-        rlEnd();
-
-        rlSetTexture(0);
+// Avoid using in prod because this is a quick testing hack
+void autogenerateRendererFromSurface(SoftBody sb, SoftBodyRenderer *rend) {
+        rend->num = sb.numSurfaces;
+        // Assumes the surface is properly connected e.t.c.
+        rend->pts = malloc(sizeof(int) * rend->num);
+        memcpy(rend->pts, sb.surfaceA, sizeof(int) * rend->num);
 }
